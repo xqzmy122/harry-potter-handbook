ee9ee1baf80bb576ff08f39b54e8ac2d7c6d9c1d
Miron <mironchik515ke@gmail.com>
2026-02-05 16:44:11 +0300

    fix: prettier

--- src/services/api/useFetch.ts
+++ src/services/api/useFetch.ts
@@ -3,24 +3,24 @@ import { useCallback, useRef, useState } from "react";
 type Status = "idle" | "loading" | "success" | "error";
 
 export const useFetch = <T>(request: () => Promise<T[]>) => {
-const [status, setStatus] = useState<Status>("idle");
-const [error, setError] = useState<string | null>(null);
-const [data, setData] = useState<T[] | null>(null);
+  const [status, setStatus] = useState<Status>("idle");
+  const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<T[] | null>(null);
 
-const requestRef = useRef(request);
-requestRef.current = request;
+  const requestRef = useRef(request);
+  requestRef.current = request;
 
-const execute = useCallback(async () => {
-try {
-setStatus("loading");
-const result = await requestRef.current();
-setData(result);
-setStatus("success");
-} catch (err) {
-err instanceof Error && setError(err.message);
-setStatus("error");
-}
-}, []);
+  const execute = useCallback(async () => {
+    try {
+      setStatus("loading");
+      const result = await requestRef.current();
+      setData(result);
+      setStatus("success");
+    } catch (err) {
+      err instanceof Error && setError(err.message);
+      setStatus("error");
+    }
+  }, []);
 
-return { execute, data, status, error };
+  return { execute, data, status, error };
 };
