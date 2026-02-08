8a83574f54c898655a8ae7f740948e9b625b7f15
Miron <mironchik515ke@gmail.com>
2026-02-08 17:04:12 +0300

    fix(prettier): CI error

--- src/services/api/spells.service.ts
+++ src/services/api/spells.service.ts
@@ -11,7 +11,7 @@ export const getSpellsByPage = async (page: number, search?: string): Promise<Sp
   const res = await api.get<ApiResponse<Spell[]>>(`/v1/spells`, {
     params: {
       "page[number]": page,
-      "page[size]": 10, 
+      "page[size]": 10,
       ...(search && { "filter[incantation_cont]": search }),
     },
   });
